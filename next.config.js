db58e60d006c670b60ab96bece223bc7df19198f
Jay Long <jaylong255@gmail.com>
2023-03-17 15:25:01 -0500

    workaround for lack of img optimization solution

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, //TODO: Remove this when we have a proper image optimization solution
+  }
 }
 
 module.exports = nextConfig
